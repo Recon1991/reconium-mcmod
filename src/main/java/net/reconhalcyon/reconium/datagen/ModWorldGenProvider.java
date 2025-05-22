83036dc68e900d392f2cb5f5beaff84677607315
Recon1991 <kalanicook@yahoo.com>
2025-05-21 20:18:22 -1000

    Created Worldgen for Ore generation.  Currently only moonstone ores

--- src/main/java/net/reconhalcyon/reconium/datagen/ModWorldGenProvider.java
+++ src/main/java/net/reconhalcyon/reconium/datagen/ModWorldGenProvider.java
@@ -6,6 +6,27 @@
 package net.reconhalcyon.reconium.datagen;
 /*╰──────────────────────────────────────────────────────  */
 
-public class ModWorldGenProvider {
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.reconhalcyon.reconium.Reconium;
+import net.reconhalcyon.reconium.worldgen.ModBiomeModifiers;
+import net.reconhalcyon.reconium.worldgen.ModConfiguredFeatures;
+import net.reconhalcyon.reconium.worldgen.ModPlacedFeatures;
 
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+public class ModWorldGenProvider extends DatapackBuiltinEntriesProvider {
+    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(ForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
+
+    public ModWorldGenProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries, BUILDER, Set.of(Reconium.MOD_ID));
+    }
 }
